5f82f2d6c5aae6df7abaea8f1953ed18177e6896
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2019-10-22 20:28:00 +0000

    Addicionado funcion get_biggest_number

--- codigos_py/dog.py
+++ codigos_py/dog.py
@@ -32,3 +32,16 @@ print("Know I have other dog which name is {}, it`s totally crazy".format(oto.na
 
 if bambi.species=="dcaniche":
     print("{0} is a {1}!".format(bambi.name, bambi.species))
+
+
+def get_biggest_number():
+    print("Please insert the list of integer numbers")
+    s = input()
+    a = list(map(int, s.split())) 
+    print("You inserted this list", a)
+    a.sort()
+    print("The biggest value is: ",a[-1])
+
+
+get_biggest_number()
+
